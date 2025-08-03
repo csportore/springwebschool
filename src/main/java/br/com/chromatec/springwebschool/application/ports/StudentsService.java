b8c8653aa9297b68866a4923116e4795091913fc
Cesar Sportore <cesar.sportore@gmail.com>
2025-08-02 22:57:21 -0300

    swc0008: Transactional methods annotated as such and fine tuning of the settings

--- src/main/java/br/com/chromatec/springwebschool/application/ports/StudentsService.java
+++ src/main/java/br/com/chromatec/springwebschool/application/ports/StudentsService.java
@@ -6,6 +6,7 @@ import br.com.chromatec.springwebschool.application.representations.StudentRepre
 import br.com.chromatec.springwebschool.infrastructure.ports.StudentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
@@ -21,6 +22,7 @@ public class StudentsService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public List<StudentRepresentation> findAll() {
         var entityList = this.repository.findAll();
         var representationList = new ArrayList<StudentRepresentation>();
@@ -28,6 +30,7 @@ public class StudentsService {
         return representationList;
     }
 
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public StudentRepresentation findById(BigInteger id) throws StudentNotFoundException {
         var entity =  this.repository.findById(id);
         return StudentMapper.INSTANCE.entityToRepresentation(entity.orElseThrow( () -> new StudentNotFoundException(
@@ -35,13 +38,13 @@ public class StudentsService {
         ) );
     }
 
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public StudentRepresentation insert(StudentRepresentation representation) {
         var entity = this.repository.save(StudentMapper.INSTANCE.representationToEntity(representation));
         return StudentMapper.INSTANCE.entityToRepresentation(entity);
     }
 
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public StudentRepresentation update(BigInteger id, StudentRepresentation representation) throws StudentNotFoundException {
         var optEntity = this.repository.findById(id);
         var entity = optEntity.orElseThrow(() -> new StudentNotFoundException(
@@ -53,7 +56,8 @@ public class StudentsService {
         this.repository.save(entity);
         return StudentMapper.INSTANCE.entityToRepresentation(entity);
     }
-    @Transactional
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public BigInteger delete(BigInteger id) throws StudentNotFoundException {
         try {
             this.repository.deleteById(id);
